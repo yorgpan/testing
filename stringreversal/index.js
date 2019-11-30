d54df08bc2a9bef3b710505a07638e0255b318a1
YorgPan <gpangiotakop@gmail.com>
2019-11-30 16:50:57 +0200

    Added another implementation of the string reversal.

--- stringreversal/index.js
+++ stringreversal/index.js
@@ -1,15 +1,28 @@
 function reverse(str) {
 
-    var reversed = '';
-    var trimmed = str.trim();
+    let reversed = '';
+    let trimmed = str.trim();
 
     for(i=trimmed.length-1; i>=0; i--){
         reversed += trimmed[i]
     }
 
     return reversed;
-
 }
 
 
-module.exports = reverse;
+
+function reverse2(str){
+
+    let reversed = '';
+    let trimmed = str.trim().split('');
+
+    return trimmed.reduce((acc, cur_val, cur_index, arr)=>{
+        return acc+arr[arr.length - cur_index - 1]
+    }, '')
+}
+
+
+
+
+module.exports = [reverse, reverse2];
--- stringreversal/test.js
+++ stringreversal/test.js
@@ -1,20 +1,23 @@
-const reverse = require('./index');
+const [reverse, reverse2] = require('./index');
 
 
 // test that reverse fn exists
 test('Reverse function exists', ()=>{
     expect(reverse).toBeDefined();
+    expect(reverse2).toBeDefined();
 });
 
 // test that reverses a string correctly
 test('Reverse function succesfully reverses a given string', ()=>{
     expect(reverse('Dog')).toEqual('goD');
+    expect(reverse2('Dog')).toEqual('goD');
 });
 
 
 // test that reverses a phrase as well
 test('Reverse function succesfully reverses a phrase as well', ()=>{
     expect(reverse('This is it')).toEqual('ti si sihT');
+    expect(reverse2('This is it')).toEqual('ti si sihT');
 });
 
 
@@ -22,6 +25,7 @@ test('Reverse function succesfully reverses a phrase as well', ()=>{
 // test that ignores whitespaces in strings
 test('Reverse function ignores whitespaces', ()=>{
     expect(reverse(' Dog ')).toEqual('goD');
+    expect(reverse2(' Dog ')).toEqual('goD');
 });
 
 
